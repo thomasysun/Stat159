474c7c85481e4b2a5aa0e552b4437e3510596c64
thomasysun <thomasysun@berkeley.edu>
2016-10-10 12:10:27 -0700

    fixed typos in test

--- Stat159Lab3/test-that/tests/tests.R
+++ Stat159Lab3/test-that/tests/tests.R
@@ -5,14 +5,14 @@ z <- c(TRUE, FALSE, TRUE)
 w <- letters[1:5]
 
 library(testthat)
-test_file("tests.R")
+test_file("tests/tests.R")
 
 # load the source code of the functions to be tested
 source("functions/range-value.R")
 
 context("Test for NA values") 
 
-test_that("range works as expected" {
+test_that("range works as expected", {
   y <- c(1, 2, 3, 4, NA)
   
   expect_length(range_value(y), 1)
@@ -21,7 +21,7 @@ test_that("range works as expected" {
 
 context("Test for TRUE/FALSE") 
 
-test_that("range works as expected" {
+test_that("range works as expected", {
   z <- c(TRUE, FALSE, TRUE)
   
   expect_equal(range_value(z), 1L)
@@ -31,7 +31,7 @@ test_that("range works as expected" {
 
 context("Test for non numbers") 
 
-test_that("range works as expected" {
+test_that("range works as expected", {
   w <- letters[1:5]
   
   expect_length(range_value(w), 1)
@@ -42,10 +42,10 @@ source("functions/missing-values.R")
 
 context("Test for sum of NA")
 
-test_that("Sum of NA works as expected" {
+test_that("Sum of NA works as expected", {
   y <- c(1, 2, 3, 4, NA)
   
-  expect_equal(range_value(x), >=0)
+  expect_gte(range_value(x), 0)
   expect_length(range_value(x), 1)
   expect_type(range_value(x), 'double')
 })
@@ -54,13 +54,12 @@ source("functions/center-measures.R")
 
 context("Test for values of center measures")
 
-test_that("Center measures work as expected" {
+test_that("Center measures work as expected", {
   x <- c(1, 2, 3, 4, 5)
   
-  expect_length(range_value(x), 2)
-  expect_type(range_value(x), 'double')
+  expect_length(center_measures(x), 2)
+  expect_type(center_measures(x), 'double')
 })
-
 
 
 
