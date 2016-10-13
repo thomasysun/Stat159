f295a0782346515aa81eb5f80edf8eccf7a63078
thomasysun <thomasysun@berkeley.edu>
2016-10-12 21:24:01 -0700

    Added files pertaining to testthat package, test-that and test-regression, w/ some dummy content. Included testthat package in session-info-script.

---
+++ stat159-fall2016-hw03/code/tests/test-regression.R
@@ -0,0 +1,60 @@
+
+x <- c(1, 2, 3, 4, 5)
+y <- c(1, 2, 3, 4, NA)
+z <- c(TRUE, FALSE, TRUE)
+w <- letters[1:5]
+
+
+# load the source code of the functions to be tested
+source("functions/regression-functions.R")
+
+context("Test for NA values") 
+
+test_that("range works as expected", {
+  y <- c(1, 2, 3, 4, NA)
+  
+  expect_length(range_value(y), 1)
+  expect_equal(range_value(y), NA_real_)
+})
+
+context("Test for TRUE/FALSE") 
+
+test_that("range works as expected", {
+  z <- c(TRUE, FALSE, TRUE)
+  
+  expect_equal(range_value(z), 1L)
+  expect_length(range_value(z), 1)
+  expect_type(range_value(z), 'integer')
+})
+
+context("Test for non numbers") 
+
+test_that("range works as expected", {
+  w <- letters[1:5]
+  
+  expect_error(range_value(w), 'non-numeric argument to binary operator')
+})
+
+source("functions/missing-values.R")
+
+context("Test for sum of NA")
+
+test_that("Sum of NA works as expected", {
+  y <- c(1, 2, 3, 4, NA)
+  
+  expect_gte(range_value(x), 0)
+  expect_length(range_value(x), 1)
+  expect_type(range_value(x), 'double')
+})
+
+source("functions/center-measures.R")
+
+context("Test for values of center measures")
+
+test_that("Center measures work as expected", {
+  x <- c(1, 2, 3, 4, 5)
+  
+  expect_length(center_measures(x), 2)
+  expect_type(center_measures(x), 'double')
+})
+
